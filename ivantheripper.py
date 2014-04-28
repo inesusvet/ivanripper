3d8afcdaaf4d7b3fa901c76101eae31a4b0b3a9d
Ivan Styazhkin <nesusvet@yandex-team.ru>
2014-04-28 11:26:15 +0400

    minor ref, +comments

--- ivantheripper.py
+++ ivantheripper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- encoding: utf-8 -*-
-'''
+"""
 Приложение для вскрытия md5 хэшей.
 Функциональные возможности:
 
@@ -8,7 +8,7 @@
     Определение кол-ва процессоров на вычислительной системе и
     параллельный запуск соответствующего кол-ва процессов перебора хэшей.
     Определение процессорного времени, затраченного на подбор хэша.
-'''
+"""
 
 from multiprocessing import Pool
 from itertools import product, ifilter, chain
@@ -17,12 +17,12 @@ from string import ascii_letters
 import hashlib
 import time
 
-rip_charset = ascii_letters
+DEFAULT_CHARSET = ascii_letters
 
 
-def gen_printable_sequence(limit=10, charset=rip_charset):
-    seq = chain.from_iterable((product(*([charset] * i))
-                              for i in xrange(1, limit + 1)))
+def gen_printable_sequence(limit=10, charset=DEFAULT_CHARSET):
+    seq = chain.from_iterable(product(*([charset] * (i + 1)))
+                              for i in xrange(limit))
     return (''.join(i) for i in seq)
 
 
@@ -43,7 +43,7 @@ def identity(arg):
 def gen_checks(md5hex, variant_sequence):
     for variant in variant_sequence:
         match = rip(variant, md5hex)
-        yield rip(variant, md5hex), variant
+        yield match, variant
         if match:
             break
 
@@ -53,18 +53,23 @@ def get_stat_info(pid):
         return statfile.read().split(' ')
 
 
+def get_cpu_time(pool):
+    return map(lambda p: map(
+        float,
+        get_stat_info(p.pid)[13:15]
+    ), pool._pool)
+
+
 def main():
     md5_passphrase = get_md5hex(raw_input('passphrase: '))
 
     pool = Pool(processes=2)
 
     time1 = time.time()
-    pool_cpu_time1 = map(lambda p: map(
-        float,
-        get_stat_info(p.pid)[13:15]
-    ), pool._pool)
+    pool_cpu_time1 = get_cpu_time(pool)
 
     # -----------------------------
+    #FIXME: WTF? Все выполняется внутри print??!
     print next(ifilter(
         lambda x: x[0],
         pool.imap(
@@ -73,7 +78,7 @@ def main():
                 md5_passphrase,
                 gen_printable_sequence()
             ),
-            chunksize=1000
+            chunksize=1024 * 4
         )
     ))
     # -----------------------------
@@ -81,10 +86,7 @@ def main():
     print 'Time spent: %f' % (time.time() - time1)
     time2 = time.time()
 
-    pool_cpu_time2 = map(lambda p: map(
-        float,
-        get_stat_info(p.pid)[13:15]
-    ), pool._pool)
+    pool_cpu_time2 = get_cpu_time(pool)
 
     for cpu_time1, cpu_time2 in zip(pool_cpu_time1, pool_cpu_time2):
         cpu_time1 = sum(cpu_time1)
